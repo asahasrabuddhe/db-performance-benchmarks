c74816dad3335cf7cc1370ee0543f84933fdb2c2
Ajitem Sahasrabuddhe <ajitem.s@outlook.com>
2023-09-02 16:31:41 -0400

    add tests for benchmarking query

--- bench_test.go
+++ bench_test.go
@@ -272,3 +272,181 @@ func BenchmarkPreparedStmtQueryRow(b *testing.B) {
 		b.Fatal(err)
 	}
 }
+
+func BenchmarkNonPreparedStmtQuery(b *testing.B) {
+	db, err := Connect()
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	err = CreateTestTable(db)
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	for i := 0; i < 1000; i++ {
+		tt := Test{Data: uuid.New().String()}
+		_, err = db.Exec("INSERT INTO test (data) VALUES (?)", tt.Data)
+		if err != nil {
+			if errors.Is(err, sql.ErrNoRows) {
+				continue
+			}
+			b.Fatal(err)
+		}
+	}
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		rowCount := rand.Int31n(1000)
+		var rows *sql.Rows
+		rows, err = db.Query("SELECT * FROM test WHERE id > ? ORDER BY RAND()", rowCount)
+		if err != nil {
+			if errors.Is(err, sql.ErrNoRows) {
+				continue
+			}
+			b.Fatal(err)
+		}
+		ttt := make([]Test, 1000-rowCount)
+		var index int
+		for rows.Next() {
+			err = rows.Scan(&ttt[index].ID, &ttt[index].Data)
+			if err != nil {
+				if errors.Is(err, sql.ErrNoRows) {
+					continue
+				}
+				b.Fatal(err)
+			}
+			index++
+		}
+	}
+
+	b.StopTimer()
+
+	err = DropTestTable(db)
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	err = db.Close()
+	if err != nil {
+		b.Fatal(err)
+	}
+}
+
+func BenchmarkNonPreparedStmtQueryGorm(b *testing.B) {
+	db, err := ConnectGORM()
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	err = CreateTestTableGORM(db, &Test{})
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	for i := 0; i < 1000; i++ {
+		tt := Test{Data: uuid.New().String()}
+		err = db.Save(&tt).Error
+		if err != nil {
+			if errors.Is(err, sql.ErrNoRows) {
+				continue
+			}
+			b.Fatal(err)
+		}
+	}
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		rowCount := rand.Int31n(1000)
+		tt := make([]Test, 1000-rowCount)
+		err = db.Where("id > ?", rowCount).Find(&tt).Error
+		if err != nil {
+			if errors.Is(err, sql.ErrNoRows) {
+				continue
+			}
+			b.Fatal(err)
+		}
+	}
+
+	b.StopTimer()
+
+	err = DropTestTableGORM(db, &Test{})
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	err = db.Close()
+	if err != nil {
+		b.Fatal(err)
+	}
+}
+
+func BenchmarkPreparedStmtQuery(b *testing.B) {
+	db, err := Connect()
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	err = CreateTestTable(db)
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	for i := 0; i < 1000; i++ {
+		tt := Test{Data: uuid.New().String()}
+		_, err = db.Exec("INSERT INTO test (data) VALUES (?)", tt.Data)
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+
+	stmt, err := db.Prepare("SELECT * FROM test WHERE id > ? ORDER BY RAND()")
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		rowCount := rand.Int31n(1000)
+		var rows *sql.Rows
+		rows, err = stmt.Query(rowCount)
+		if err != nil {
+			if errors.Is(err, sql.ErrNoRows) {
+				continue
+			}
+			b.Fatal(err)
+		}
+		ttt := make([]Test, 1000-rowCount)
+		var index int
+		for rows.Next() {
+			err = rows.Scan(&ttt[index].ID, &ttt[index].Data)
+			if err != nil {
+				if errors.Is(err, sql.ErrNoRows) {
+					continue
+				}
+				b.Fatal(err)
+			}
+			index++
+		}
+	}
+
+	b.StopTimer()
+
+	err = stmt.Close()
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	err = DropTestTable(db)
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	err = db.Close()
+	if err != nil {
+		b.Fatal(err)
+	}
+}
